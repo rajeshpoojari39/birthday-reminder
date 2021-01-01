e7024c6a0cb2baa223915921de2dddd7cfad36e5
Rajesh Poojari <rajeshpoojari39@gmail.com>
2021-01-01 20:49:14 +0530

    Initial Commit

---
+++ src/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <section key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </section>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
